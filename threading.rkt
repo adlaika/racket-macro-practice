546c297f2f3e27ee90108bb91e15445b16e92055
Michael Arnold <marnold@cj.com>
2017-03-07 12:40:32 -0800

    use syntax class instead of list wrapper to determine id-ness

--- threading.rkt
+++ threading.rkt
@@ -5,30 +5,30 @@
 
 (require rackunit)
 
-(define-syntax (~~> stx)
+(define-syntax (~> stx)
   (syntax-parse stx
     [(_ x)
      #'x]
     [(_ x (f a ...) more ...)
-     #'(~~> (f x a ...) more ...)]
-    [(_ x id more ...)
-     #'(~~> x (id) more ...)]
+     #'(~> (f x a ...) more ...)]
+    [(_ x id:id more ...)
+     #'(~> x (id) more ...)]
     ))
 
-(check-equal? (~~> 'hello) 'hello)
-(check-equal? (~~> (+ 1 2)) 3)
-(check-equal? (~~> (let ([x 12])
+(check-equal? (~> 'hello) 'hello)
+(check-equal? (~> (+ 1 2)) 3)
+(check-equal? (~> (let ([x 12])
                      (* x 3))) 36)
 
-(check-equal?  (~~> 5 (+ 1)) 6)
-(check-equal? (~~> 5 (+ 1) (* 2)) 12)
+(check-equal?  (~> 5 (+ 1)) 6)
+(check-equal? (~> 5 (+ 1) (* 2)) 12)
 
-(check-equal? (~~> 1 number->string) "1")
-(check-equal? (~~> #\a
+(check-equal? (~> 1 number->string) "1")
+(check-equal? (~> #\a
                    (list #\z)
                    list->string)
               "az")
-(check-equal?(~~> 'abc
+(check-equal?(~> 'abc
                  symbol->string
                  string->bytes/utf-8
                  (bytes-ref 1)
